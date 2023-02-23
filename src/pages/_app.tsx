13ea296ec54d482d6307c40b23dd3e1a7c82dff9
Armaan Gupta <armaan@guppy.im>
2023-02-23 10:13:19 -0600

    feat: setup react-cookie and react-query

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,16 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import '@/styles/globals.css';
+import type { AppProps } from 'next/app';
+import { CookiesProvider } from 'react-cookie';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+const queryClient = new QueryClient();
+
+export default function App({ Component, pageProps }: AppProps) {4
+  return (
+    <QueryClientProvider client={queryClient}>
+      <CookiesProvider>
+        <Component {...pageProps} />
+      </CookiesProvider>
+    </QueryClientProvider>
+  )
 }
