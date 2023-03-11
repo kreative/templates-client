462844d6a5ef57cab9a34e61093fe62f050f34ce
Armaan Gupta <armaan@guppy.im>
2023-03-10 21:19:16 -0600

    feat: added redirect from index

--- next.config.js
+++ next.config.js
@@ -3,4 +3,16 @@ const nextConfig = {
   reactStrictMode: true,
 }
 
-module.exports = nextConfig
+module.exports = nextConfig;
+
+module.exports = {
+  async redirects() {
+    return [
+      {
+        source: '/',
+        destination: '/obsidian',
+        permanent: true,
+      },
+    ]
+  }
+}
