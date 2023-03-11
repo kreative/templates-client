2b78c6e920752150a5051178981a6551a9d1a80c
Armaan Gupta <armaan@guppy.im>
2023-03-10 21:19:49 -0600

    feat: added hydration

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,7 @@
 import '@/styles/globals.css';
 import type { AppProps } from 'next/app';
 import { CookiesProvider } from 'react-cookie';
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { Hydrate, QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
 const queryClient = new QueryClient();
 
@@ -9,7 +9,9 @@ export default function App({ Component, pageProps }: AppProps) {4
   return (
     <QueryClientProvider client={queryClient}>
       <CookiesProvider>
-        <Component {...pageProps} />
+        <Hydrate state={pageProps.dehydratedState}>
+          <Component {...pageProps} />
+        </Hydrate>
       </CookiesProvider>
     </QueryClientProvider>
   )
