c899b4783cf1798fe0efa7af97264cfea7910018
Armaan Gupta <armaan@guppy.im>
2023-03-19 16:21:53 -0500

    feat: configured custom Craftwork Sans font

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,18 +1,62 @@
-import '@/styles/globals.css';
-import type { AppProps } from 'next/app';
-import { CookiesProvider } from 'react-cookie';
-import { Hydrate, QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import "@/styles/globals.css";
+import type { AppProps } from "next/app";
+import { CookiesProvider } from "react-cookie";
+import localFont from "@next/font/local";
+import {
+  Hydrate,
+  QueryClient,
+  QueryClientProvider,
+} from "@tanstack/react-query";
+
+// custom craftwork sans font loading locally
+const craftworkSans = localFont({
+  src: [
+    {
+      path: "../../public/fonts/CraftworkSans/CraftworkSans-Regular.woff",
+      weight: "400",
+      style: "normal",
+    },
+    {
+      path: "../../public/fonts/CraftworkSans/CraftworkSans-Regular.woff2",
+      weight: "400",
+      style: "normal",
+    },
+    {
+      path: "../../public/fonts/CraftworkSans/CraftworkSans-Regular.ttf",
+      weight: "400",
+      style: "normal",
+    },
+    {
+      path: "../../public/fonts/CraftworkSans/CraftworkSans-Bold.woff",
+      weight: "700",
+      style: "normal",
+    },
+    {
+      path: "../../public/fonts/CraftworkSans/CraftworkSans-Bold.woff2",
+      weight: "700",
+      style: "normal",
+    },
+    {
+      path: "../../public/fonts/CraftworkSans/CraftworkSans-Bold.ttf",
+      weight: "700",
+      style: "normal",
+    },
+  ],
+  variable: "--font-craftworksans",
+});
 
 const queryClient = new QueryClient();
 
-export default function App({ Component, pageProps }: AppProps) {4
+export default function App({ Component, pageProps }: AppProps) {
   return (
     <QueryClientProvider client={queryClient}>
       <CookiesProvider>
         <Hydrate state={pageProps.dehydratedState}>
-          <Component {...pageProps} />
+          <div className={`${craftworkSans.variable} font-sans`}>
+            <Component {...pageProps} />
+          </div>
         </Hydrate>
       </CookiesProvider>
     </QueryClientProvider>
-  )
+  );
 }
