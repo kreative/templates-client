e414ed313ce7129ea2247f1c9ab55773f31b9774
Armaan Gupta <armaan@guppy.im>
2023-03-19 17:42:36 -0500

    ref: configured hostnames for images

--- next.config.js
+++ next.config.js
@@ -1,18 +1,31 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
-
-module.exports = nextConfig;
-
-module.exports = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "tailwindui.com",
+        port: "",
+        pathname: "/**",
+      },
+      {
+        protocol: "https",
+        hostname: "cdn.kreativeusa.com",
+        port: "",
+        pathname: "/**",
+      },
+    ],
+  },
   async redirects() {
     return [
       {
-        source: '/',
-        destination: '/obsidian',
+        source: "/",
+        destination: "/obsidian",
         permanent: true,
       },
-    ]
-  }
-}
+    ];
+  },
+};
+
+module.exports = nextConfig;
