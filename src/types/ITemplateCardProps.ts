ce79a23a4580da318ee8305fd18c474feb9bbe85
Armaan Gupta <armaan@guppy.im>
2023-03-19 23:46:38 -0500

    feat: created template card props interface

---
+++ src/types/ITemplateCardProps.ts
@@ -0,0 +1,10 @@
+export interface ITemplateCardProps {
+  name: string;
+  authorName: string;
+  authorAvatar: string;
+  price: string;
+  tagline: string;
+  thumbnailUrl: string;
+  slug: string;
+  categoryName: string;
+}
