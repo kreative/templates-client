79c16b8061e076a297f1cabf8751e734d2268de2
Armaan Gupta <armaan@guppy.im>
2023-03-23 02:28:19 -0500

    ref: changed styling

--- src/components/AuthorBox.tsx
+++ src/components/AuthorBox.tsx
@@ -10,12 +10,12 @@ const AuthorBox: React.FC<IAuthorBoxProps> = (
         <Image
           src={props.avatarUrl}
           alt={`${props.displayName} avatar image`}
-          className="w-16 h-16 rounded-full"
+          className="w-14 h-14 rounded-full"
           width={48}
           height={48}
         />
         <div className="ml-4">
-          <p className="text-lg text-gray-900">{props.displayName}</p>
+          <p className="text-xl text-gray-900 pb-1">{props.displayName}</p>
           <p className="text-md text-gray-600">{props.bio}</p>
         </div>
       </div>
