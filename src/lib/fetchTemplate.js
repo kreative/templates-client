611d7e1f07bce63a7b0428301fcb4669395511bc
Armaan Gupta <armaan@guppy.im>
2023-03-23 01:03:44 -0500

    feat: added fetch template by slug api call

---
+++ src/lib/fetchTemplate.js
@@ -0,0 +1,5 @@
+export default async function fetchTemplate(slug) {
+  const res = await fetch(`https://api.kreativetemplates.co/v1/templates/${slug}`);
+  const payload = await res.json();
+  return payload.data;
+}
