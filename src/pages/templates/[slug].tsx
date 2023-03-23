a34f491eebc06029837675fd4d2b7bfe1929f7b7
Armaan Gupta <armaan@guppy.im>
2023-03-23 01:03:58 -0500

    ref: getting template by slug not id

---
+++ src/pages/templates/[slug].tsx
@@ -0,0 +1,56 @@
+import Head from "next/head";
+import Image from "next/image";
+import Link from "next/link";
+import { useRouter } from "next/router";
+import { dehydrate, QueryClient, useQuery } from "@tanstack/react-query";
+import Navbar from "../../components/Navbar";
+import Footer from "../../components/Footer";
+import fetchTemplate from "../../lib/fetchTemplate";
+
+export default function SingleTemplate(props: any): JSX.Element {
+  const router = useRouter();
+  const { slug } = router.query;
+  const { data } = useQuery(["template", slug], () => {
+    return fetchTemplate(slug);
+  });
+
+  return (
+    <div>
+      <Head>
+        <title>{data.name} - Kreative Templates</title>
+        <meta
+          name="description"
+          content={`${data.name} - ${data.tagline} - ${data.description}`}
+        />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <Navbar />
+      <main>
+        <div className="mx-auto max-w-7xl px-6 pt-10" aria-label="Top">
+          {!data && <div>No template found</div>}
+          {data && (
+            <div>
+              {data.name}
+            </div>
+          )}
+        </div>
+      </main>
+      <Footer />
+    </div>
+  );
+}
+
+export async function getServerSideProps(context: any) {
+  const { slug } = context.params;
+  const queryClient = new QueryClient();
+
+  await queryClient.prefetchQuery(["template", slug], () => {
+    return fetchTemplate(slug);
+  });
+
+  return {
+    props: {
+      dehydratedState: dehydrate(queryClient),
+    },
+  };
+}
