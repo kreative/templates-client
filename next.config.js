790af84716ec36e6195c38c6b278ed400c3d9689
Armaan Gupta <armaan@guppy.im>
2023-04-06 11:05:18 -0500

    fix: bundle analyzer working

--- next.config.js
+++ next.config.js
@@ -43,5 +43,4 @@ const withBundleAnalyzer = require('@next/bundle-analyzer')({
 		enabled: process.env.ANALYZE === 'true',
 });
 
-module.exports = withBundleAnalyzer({});
-module.exports = nextConfig;
+module.exports = withBundleAnalyzer(nextConfig);
