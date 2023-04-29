ddf5bb70554471dfd19a8ae73445bff8cd468b98
Armaan Gupta <armaan@guppy.im>
2023-04-29 01:11:02 -0500

    feat: added hover and tap animations

--- src/components/TemplateCard.tsx
+++ src/components/TemplateCard.tsx
@@ -7,6 +7,7 @@ import { Cloudinary } from "@cloudinary/url-gen";
 import { fill } from "@cloudinary/url-gen/actions/resize";
 import { AdvancedImage } from "@cloudinary/react";
 import { autoGravity } from "@cloudinary/url-gen/qualifiers/gravity";
+import { motion } from "framer-motion";
 
 interface ITemplateCardProps {
   name: string;
@@ -20,7 +21,7 @@ interface ITemplateCardProps {
 }
 
 const TemplateCard: React.FC<ITemplateCardProps> = (
-  props: ITemplateCardProps
+  props: ITemplateCardProps,
 ): JSX.Element => {
   const [isHovering, setIsHovering] = useState(false);
 
@@ -34,53 +35,60 @@ const TemplateCard: React.FC<ITemplateCardProps> = (
   thumbnail.resize(fill().width(600).height(400).gravity(autoGravity()));
 
   return (
-    <div
-      className="hvr-grow pb-4"
-      onMouseEnter={() => setIsHovering(true)}
-      onMouseLeave={() => setIsHovering(false)}
+    <motion.div
+      whileHover={{
+        scale: 1.03,
+        transition: { duration: 0.2 },
+      }}
+      whileTap={{ scale: 0.97 }}
     >
-      <Link href={`/templates/${props.slug}`}>
-        <AdvancedImage
-          cldImg={thumbnail}
-          className="w-full h-auto rounded-lg border border-gray-300"
-          alt={`${props.name} template thumbnail image`}
-          
-        />
-      </Link>
-      <div className="flex items-start pt-4">
-        <Image
-          className="w-12 h-12 rounded-full"
-          src={props.authorAvatar}
-          alt={props.authorName}
-          width={48}
-          height={48}
-          placeholder="blur"
-          blurDataURL={props.authorAvatar}
-        />
-        <div className="ml-4">
-          <Link href={`/templates/${props.slug}`}>
-            <h2>
-              <span className="text-xl font-bold hover:text-purple-700">
-                {props.name}
-              </span>{" "}
-              <span className={isHovering ? "inline-block" : "hidden"}>
-                <span className="text-gray-500">for</span>{" "}
-                {normalizePrice(props.price)}
-              </span>
-            </h2>
-          </Link>
-          <p>
-            by {props.authorName} <span className="text-gray-500">in</span>{" "}
-            <Link
-              href={`/categories/${props.categoryName}`}
-              className="underline hover:text-purple-500"
-            >
-              {normalizeText(props.categoryName)}
+      <div
+        className="hvr-grow pb-4"
+        onMouseEnter={() => setIsHovering(true)}
+        onMouseLeave={() => setIsHovering(false)}
+      >
+        <Link href={`/templates/${props.slug}`}>
+          <AdvancedImage
+            cldImg={thumbnail}
+            className="w-full h-auto rounded-lg border border-gray-300"
+            alt={`${props.name} template thumbnail image`}
+          />
+        </Link>
+        <div className="flex items-start pt-4">
+          <Image
+            className="w-12 h-12 rounded-full"
+            src={props.authorAvatar}
+            alt={props.authorName}
+            width={48}
+            height={48}
+            placeholder="blur"
+            blurDataURL={props.authorAvatar}
+          />
+          <div className="ml-4">
+            <Link href={`/templates/${props.slug}`}>
+              <h2>
+                <span className="text-xl font-bold hover:text-purple-700">
+                  {props.name}
+                </span>{" "}
+                <span className={isHovering ? "inline-block" : "hidden"}>
+                  <span className="text-gray-500">for</span>{" "}
+                  {normalizePrice(props.price)}
+                </span>
+              </h2>
             </Link>
-          </p>
+            <p>
+              by {props.authorName} <span className="text-gray-500">in</span>{" "}
+              <Link
+                href={`/categories/${props.categoryName}`}
+                className="underline hover:text-purple-500"
+              >
+                {normalizeText(props.categoryName)}
+              </Link>
+            </p>
+          </div>
         </div>
       </div>
-    </div>
+    </motion.div>
   );
 };
 
